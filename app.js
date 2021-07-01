5a824aafb7c2554dcbc34a85106fa42b808e9651
Shaeyi-lu <57910040+Shaeyi-lu@users.noreply.github.com>
2021-07-01 19:44:26 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 
+const http = require('http');
+
+const port = process.env.PORT || 5000;
+
+const server = http.createServer((request, response) => {
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
+  response.end('Hello World\n\nCool!');
+});
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('Something went wrong ', err);
+  }
+  console.log('Server Listening on: ${port}');
+  
+});
