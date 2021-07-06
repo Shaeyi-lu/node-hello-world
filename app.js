20718cbe53573b237493d8b1410fcd06e7def42c
Mercy Lelago <59580006+mercyy-maker@users.noreply.github.com>
2021-07-05 18:30:06 -0700

    Worked on index page and Navigation bar

--- app.js
+++ app.js
@@ -1,18 +1,41 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const http = require('http');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-const port = process.env.PORT || 5000;
+var app = express();
 
-const server = http.createServer((request, response) => {
-  response.statusCode = 200;
-  response.setHeader('Content-Type', 'text/plain');
-  response.end('Hello World\n\nCool!');
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-server.listen(port, (err) => {
-  if (err) {
-    return console.log('Something went wrong ', err);
-  }
-  console.log('Server Listening on: ${port}');
-  
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
+
+module.exports = app;
